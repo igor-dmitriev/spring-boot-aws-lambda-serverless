17d97e1113da09716b875b5e1231014cdefe922c
idmitriiev <ihor.dmitriiev@gmail.com>
2024-06-14 15:15:02 +0300

    add tracing

--- src/main/java/com/amazonaws/serverless/sample/springboot3/controller/UserController.java
+++ src/main/java/com/amazonaws/serverless/sample/springboot3/controller/UserController.java
@@ -3,11 +3,15 @@ package com.amazonaws.serverless.sample.springboot3.controller;
 import com.amazonaws.serverless.sample.springboot3.entity.UserEntity;
 import com.amazonaws.serverless.sample.springboot3.repository.UserRepository;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+  private static final Logger log = LogManager.getLogger();
 
   private final UserRepository userRepository;
 
@@ -17,6 +21,11 @@ public class UserController {
 
   @GetMapping("/users")
   public List<UserEntity> getAllUsers() {
-    return userRepository.findAll();
+    StopWatch stopWatch = new StopWatch();
+    stopWatch.start();
+    List<UserEntity> users = userRepository.findAll();
+    stopWatch.stop();
+    log.info("userRepository findAll elapsed time: {}", stopWatch.getTotalTimeMillis());
+    return users;
   }
 }
