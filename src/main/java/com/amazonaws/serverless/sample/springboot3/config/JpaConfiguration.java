b0fc5b4f9004ac30d487f00c06036f58d47a6a93
idmitriiev <ihor.dmitriiev@gmail.com>
2024-06-14 13:41:20 +0300

    hikari datasource

--- src/main/java/com/amazonaws/serverless/sample/springboot3/config/JpaConfiguration.java
+++ src/main/java/com/amazonaws/serverless/sample/springboot3/config/JpaConfiguration.java
@@ -1,31 +1,37 @@
 package com.amazonaws.serverless.sample.springboot3.config;
 
 import com.p6spy.engine.spy.P6DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class JpaConfiguration {
 
   public DataSource dataSource() {
-    var dataSource = new SimpleDriverDataSource();
-    dataSource.setDriverClass(org.postgresql.Driver.class);
-    dataSource.setUrl("jdbc:postgresql://" + System.getenv("DB_HOST") + ":5432/spring-lambda");
-    dataSource.setUsername(System.getenv("DB_LOGIN"));
-    dataSource.setPassword(System.getenv("DB_PASSWORD"));
-    return new CustomDatasource(new P6DataSource(dataSource));
+    HikariConfig hikariConfig = new HikariConfig();
+    hikariConfig.setDriverClassName(org.postgresql.Driver.class.getName());
+    hikariConfig.setJdbcUrl("jdbc:postgresql://" + System.getenv("DB_HOST") + ":5432/spring-lambda");
+    hikariConfig.setUsername(System.getenv("DB_LOGIN"));
+    hikariConfig.setPassword(System.getenv("DB_PASSWORD"));
+    hikariConfig.setConnectionTestQuery("SELECT 1");
+    hikariConfig.setPoolName("springHikariCP");
+    return new CustomDatasource(new P6DataSource(new HikariDataSource(hikariConfig)));
   }
 
   @Bean
-  public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+  public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
     return new JpaTransactionManager(emf);
   }
 
